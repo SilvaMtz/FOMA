35730421b2ac270a1f4b92712f5a9cbd4e886f36
David Martinez <dmtzds@gmail.com>
2018-11-21 05:50:15 -0600

    test

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 git clone https://github.com/SilvaMtz/FOMA.git ~/.foma
+
 foma=$(echo $PATH | grep foma)
 
 if [ -z $foma ]; then
-    echo 'Installation in progress...'
+    echo 'Adding foma to your path...'
     echo 'export PATH="$HOME/.foma:$PATH"' >> ~/.bash_profile
     echo 'export PATH="$HOME/.foma:$PATH"' >> ~/.bashrc
     echo 'export PATH="$HOME/.foma:$PATH"' >> ~/.zshrc
 fi
 
-echo 'Installation successful!'
-echo 'Please reset the terminal!'
+echo 'FINISHED!'

2992a16de6065d70cf18506ece69ed1cb4fd9e5a
David Martinez <dmtzds@gmail.com>
2018-11-21 05:56:39 -0600

    shell fix

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@ git clone https://github.com/SilvaMtz/FOMA.git ~/.foma
 foma=$(echo $PATH | grep foma)
 
 if [ -z $foma ]; then
-    echo 'Adding foma to your path...'
+    echo 'Installation in progress...'
     echo 'export PATH="$HOME/.foma:$PATH"' >> ~/.bash_profile
     echo 'export PATH="$HOME/.foma:$PATH"' >> ~/.bashrc
     echo 'export PATH="$HOME/.foma:$PATH"' >> ~/.zshrc
 fi
 
-echo 'FINISHED!'
+echo 'FINISHED! Now reset the terminal!'
